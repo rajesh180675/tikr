a4674436387ede698b742a462ba1eec83a9c2ffd
rajesh180675 <rajesh.l.mehta@gmail.com>
2025-07-09 00:41:26 +0530

    Update tikr_dashboard_streamlit (1).py

--- tikr_dashboard_streamlit (1).py
+++ tikr_dashboard_streamlit (1).py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seab as sns
+import seaborn as sns
 from datetime import datetime, timedelta
 import warnings
 warnings.filterwarnings("ignore")
